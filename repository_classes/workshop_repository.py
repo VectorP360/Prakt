ff345615b29e8f9758e946fb2c44afaabb1ba8a7
user <>
2025-06-05 14:46:33 +0300

    refactor: переделал принцип работы методов

--- repository_classes/workshop_repository.py
+++ repository_classes/workshop_repository.py
@@ -7,12 +7,13 @@ class WorkshopRepository:
 
     def create(self, workshop_id: int, workshop_name: str):
         cursor = self.__conn.cursor()
-
-        cursor.execute(f'''
-        INSERT INTO workshop (workshop_id, workshop_name) VALUES ({workshop_id}, '{workshop_name}');
-        ''')
-        
-        self.__conn.commit()
+        try:
+            cursor.execute(f'''
+            INSERT INTO workshop (workshop_id, workshop_name) VALUES ({workshop_id}, '{workshop_name}');
+            ''')
+            self.__conn.commit()
+        except psycopg.errors.UniqueViolation: #Ошибка при создании записи (Повторение ключа)
+            print('Запись с данным ID уже существует')
 
         return workshop.Workshop(workshop_id, workshop_name)
 
@@ -21,20 +22,31 @@ class WorkshopRepository:
         cursor = self.__conn.cursor()
 
         cursor.execute(f'''
-        SELECT * FROM workshop WHERE workshop_id = {workshop_id};
+        SELECT * FROM workshop WHERE workshop_ID = {workshop_id};
         ''')
 
-        return cursor.fetchall()
+        result = cursor.fetchone()
+        if result != None:
+            new_obj = workshop.Workshop(result[0],result[1])
+            return new_obj.__dict__
+        else:
+            return 'Записи с данным ID не обнаружено'
+
     
     def get_all(self):
         cursor = self.__conn.cursor()
 
         cursor.execute(f'''
-        SELECT * FROM workshop;
+        SELECT * FROM workshop ORDER BY workshop_ID;
         ''')
         
-        result = cursor.fetchall()
+        query = cursor.fetchall()
+        result = []
                 
+        for iteration in query:
+            new_obj = workshop.Workshop(iteration[0],iteration[1])
+            result.append(new_obj.__dict__)
+
         return result
 
     def update(self, workshop_id, new_name):
@@ -48,5 +60,8 @@ class WorkshopRepository:
     def delete(self, workshop_id: int):
         cursor = self.__conn.cursor()
 
-        cursor.execute(f'''DELETE FROM workshop WHERE workshop_ID = {workshop_id} ''')
-        self.__conn.commit()
+        try:
+            cursor.execute(f'''DELETE FROM workshop WHERE workshop_ID = {workshop_id} ''')
+            self.__conn.commit()
+        except psycopg.errors.UniqueViolation: 
+            print('Записи с данным ID не было обнаружено')
