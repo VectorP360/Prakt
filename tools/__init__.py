1e532a25f8bf3d947e51f9c3f252f17d083dc001
Tamerlan <TAGlazunov@vnikti.rosneft.ru>
2025-06-20 11:35:24 +0300

    refactor: Вынес вспомогательные классы в Tools

---
+++ tools/__init__.py
@@ -0,0 +1,8 @@
+from .excel_reader import NewUsersExcelReader
+from .password_generator import PasswordGenerator
+
+
+__all__ = [
+    'PasswordGenerator', 
+    'NewUsersExcelReader'
+    ]
---
+++ tools/password_generator.py
@@ -0,0 +1,34 @@
+from random import sample
+import string
+
+
+class PasswordGenerator:
+    symbols = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+    
+    @staticmethod
+    def generate(password_length: int): 
+        used_letters = False
+        used_capital = False
+        used_numbers = False
+        used_symbols = False
+
+        while not used_letters or not used_capital or not used_numbers or not used_symbols:
+            used_letters = False
+            used_capital = False
+            used_numbers = False
+            used_symbols = False
+
+            final_password = "".join(sample(PasswordGenerator.symbols , password_length))
+
+            for iteration in final_password:
+                if iteration in string.ascii_lowercase:
+                    used_letters = True
+                if iteration in string.ascii_uppercase:
+                    used_capital = True
+                if iteration in string.digits:
+                    used_numbers = True
+                if iteration in string.punctuation:
+                    used_symbols = True
+
+        return final_password
+    
